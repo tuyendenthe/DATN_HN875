847b469c75203f6febf5b8d8ad9ca24817487d01
duy-loi <loildph37681@fpt.edu.vn>
2024-09-28 13:02:49 +0700

    tim kiem san pham

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function search(Request $req) {
+        $products = Product::where('name', 'like', '%'. $req->keyw . '%')->get();
+
+        return view('clients.shop', compact('products'));
+    }
+}
