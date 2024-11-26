5c7b4797518860d5b7514da6b0fe9558499ee220
tuyendenthe <hvt910tranvantuyen@gmail.com>
2024-11-26 23:16:54 +0700

    ok

--- database/migrations/2024_09_21_100010_products.php
+++ database/migrations/2024_09_21_100010_products.php
@@ -20,13 +20,18 @@ return new class extends Migration
             $table->integer('price');
             $table->string('image');
             $table->string('content');
+            $table->string('chip'); // thêm loại chip cho sản phẩm
+            $table->string('ram');
+            $table->string('memory'); //bộ nhớ
+            $table->string('screen');  //kích thước màn
+            $table->string('resolution'); // độ phân giải
             $table->string('content_short');
             $table->integer('role')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
     }
-    
+
 
     /**
      * Reverse the migrations.
